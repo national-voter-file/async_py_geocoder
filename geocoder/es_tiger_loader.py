face119b915207f8a411c87a5bbab0f8accff059
Patrick Sier <pjsier@users.noreply.github.com>
2017-05-21 13:00:31 -0500

    working for building bucket, joining to place files, and querying. might change city to must (#2)

--- geocoder/es_tiger_loader.py
+++ geocoder/es_tiger_loader.py
@@ -11,6 +11,9 @@ import pyproj
 from random import SystemRandom
 from itertools import chain
 from elasticsearch import Elasticsearch, helpers
+from rtree import index
+from shapely.geometry import Polygon
+
 
 CURRENT_DIR = os.path.dirname(__file__)
 
@@ -78,6 +81,31 @@ tiger_settings = {
 }
 
 
+def make_place_rtree(state_str):
+    fips_state = fips_state_map[state_str]
+    place_obj = s3.Object('nvf-tiger-2016', 'PLACE/tl_2016_{}_place.zip'.format(fips_state))
+    place_bytes = BytesIO(place_obj.get()['Body'].read())
+    place_zip = ZipFile(place_bytes)
+    place_shp = shapefile.Reader(
+        shp=BytesIO(place_zip.read('tl_2016_{}_place.shp'.format(fips_state))),
+        dbf=BytesIO(place_zip.read('tl_2016_{}_place.dbf'.format(fips_state)))
+    )
+
+    place_idx = index.Index()
+    place_map = {}
+    name_idx = list(filter(lambda x: x[1][0] == 'NAME', enumerate(place_shp.fields)))[0][0]
+
+    for idx, shape in enumerate(place_shp.shapeRecords()):
+        place_map[idx] = {
+            'shape': shape,
+            'name': shape.record[name_idx],
+            'geom': Polygon(shape.shape.points)
+        }
+        sh = shape.shape.bbox
+        place_idx.insert(idx, (sh[1], sh[0], sh[3], sh[2]))
+    return place_map, place_idx
+
+
 def process_zip(obj):
     tiger_key = obj.key[3:-4]
     tiger_bytes = BytesIO(obj.get()['Body'].read())
@@ -88,7 +116,12 @@ def process_zip(obj):
     )
 
 
-def process_records(reader, state_str):
+def make_bbox_poly(bbox):
+    return Polygon([(bbox[0], bbox[1]), (bbox[0], bbox[3]),
+                    (bbox[2], bbox[3]), (bbox[2], bbox[1])])
+
+
+def process_records(reader, place_idx, state_str):
     field_names = [f[0] for f in reader.fields[1:]]
     feature_list = list()
 
@@ -100,6 +133,15 @@ def process_records(reader, state_str):
                 atr[k] = atr[k].decode('utf-8').strip()
 
         atr['STATE'] = state_str
+        sh = sr.shape.bbox
+        for fid in place_idx.intersection([sh[1], sh[0], sh[3], sh[2]]):
+            line_box = make_bbox_poly(sh)
+            if not line_box.is_valid:
+                continue
+            if line_box.intersects(place_map[fid]['geom']):
+                atr['PLACE'] = place_map[fid]['name']
+                break
+
         geom = sr.shape.__geo_interface__
         geom['coordinates'] = [
             pyproj.transform(nad83, wgs84, p[0], p[1]) for p in geom['coordinates']
@@ -126,10 +168,12 @@ if __name__ == '__main__':
     else:
         state_str = args.upper()
         prefix = fips_state_map[state_str]
-        prefix_str += '/'
+        prefix_str = prefix + '/'
+
+    place_map, place_idx = make_place_rtree(state_str)
 
     # Generator expression for pulling ADDRFEAT data for a single state
-    zip_yield = (process_records(process_zip(r), state_str)
+    zip_yield = (process_records(process_zip(r), place_idx, state_str)
                  for r in bucket.objects.filter(Prefix=prefix_str))
 
     # Generator expression unpacking sublist and yielding ES object
